db46429a8c310e0988b2ff6b56e769a52ccc2da5
mawule-gabriel <mawulegabriel@gmail.com>
2026-02-23 23:54:19 +0000

    Initial commit

---
+++ src/main/java/com/ghana/commoditymonitor/CommodityMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.ghana.commoditymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommodityMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommodityMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghana/commoditymonitor/CommodityMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghana.commoditymonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommodityMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
